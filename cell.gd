6a644bb61f0ff7b08ac5d5306ebfbd04e84488b1
Maks <maks.razrushitel08@gmail.com>
2023-05-06 14:42:04 +0300

    Gudoku v1.0.0: Minimal functional avalible

---
+++ cell.gd
@@ -0,0 +1,44 @@
+extends Button
+
+var numbers : Array[int] = []
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _input(event):
+	if !has_focus():
+		return;
+	for i in range(1, 10):
+		if Input.is_action_just_released(str(i)):
+			if i in numbers:
+				numbers.erase(i)
+			else:
+				numbers.push_back(i)
+	if (Input.is_action_pressed("erase")):
+		numbers.pop_back()
+
+	_update_text()
+
+
+func _update_text():
+	numbers.sort()
+	text = ""
+
+	for i in range(0, numbers.size()):
+		text += "%d," % numbers[i]
+		if !(i+1) % 3:
+			text += "\n"
+		else:
+			text += " "
+
+	if text.length() >= 2:
+		text = text.left(-2)
+
+	if text.length() == 1:
+		var big_font_size := theme.get_font_size("big_font_size", "Button")
+		add_theme_font_size_override("font_size", big_font_size)
+	else:
+		remove_theme_font_size_override("font_size")
---
+++ field.gd
@@ -0,0 +1,10 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var vals : Array[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	# $Square1/Line1/Cell1.numbers = vals
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
