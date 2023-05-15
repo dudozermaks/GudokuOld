3737fc638196da903d8add8e5777eecca81945be
Maks <maks.razrushitel08@gmail.com>
2023-05-15 19:16:50 +0300

    rename files and remove magic numbers

--- field.gd
+++ field.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends VBoxContainer 
 
 enum VALIDATE{
 	WRONG_SOLVED,
@@ -6,13 +6,16 @@ enum VALIDATE{
 	UNSOLVED
 }
 
+const board_size = 9
+const square_size = 3
+
 func _ready():
-	%Field.get_node("Line0/Cell0").grab_focus()
+	get_node("Line0/Cell0").grab_focus()
 
 func _is_unsolved() -> bool:
-	for i in range(0, 9):
-		var line := %Field.get_node("Line" + str(i))
-		for j in range(0, 9):
+	for i in range(0, board_size):
+		var line := get_node("Line" + str(i))
+		for j in range(0, board_size):
 			if line.get_node("Cell" + str(j)).numbers.size() != 1:
 				return true
 	return false
@@ -21,8 +24,8 @@ func _is_unsolved() -> bool:
 func _validate_line(line_number : int) -> VALIDATE:
 		var numbers : Array[int] = []
 
-		var line := %Field.get_node("Line" + str(line_number))
-		for i in range(0, 9):
+		var line := get_node("Line" + str(line_number))
+		for i in range(0, board_size):
 			var cell : Cell = line.get_node("Cell" + str(i))
 
 			if cell.numbers.size() != 1: continue
@@ -37,8 +40,8 @@ func _validate_line(line_number : int) -> VALIDATE:
 func _validate_row(row_number : int) -> VALIDATE:
 		var numbers : Array[int] = []
 
-		for i in range(0, 9):
-			var line := %Field.get_node("Line" + str(i))
+		for i in range(0, board_size):
+			var line := get_node("Line" + str(i))
 			var cell : Cell = line.get_node("Cell" + str(row_number))
 			
 			if cell.numbers.size() != 1: continue
@@ -53,14 +56,13 @@ func _validate_row(row_number : int) -> VALIDATE:
 func _validate_square(square_number : int) -> VALIDATE:
 		var numbers : Array[int] = [] 
 
-		# first_line_number either 0, 3 or 9
 		@warning_ignore("integer_division")
-		var first_line_number = (square_number / 3) * 3
-		var first_cell_number = (square_number % 3) * 3
+		var first_line_number = (square_number / square_size) * square_size
+		var first_cell_number = (square_number % square_size) * square_size
 
-		for line_number in range(first_line_number, first_line_number + 3):
-			var line = %Field.get_node("Line" + str(line_number))
-			for cell_number in range(first_cell_number, first_cell_number + 3):
+		for line_number in range(first_line_number, first_line_number + square_size):
+			var line = get_node("Line" + str(line_number))
+			for cell_number in range(first_cell_number, first_cell_number + square_size):
 				var cell = line.get_node("Cell" + str(cell_number))
 
 				if cell.numbers.size() != 1: continue
@@ -72,7 +74,7 @@ func _validate_square(square_number : int) -> VALIDATE:
 		return VALIDATE.RIGHT_SOLVED
 
 func _validate() -> VALIDATE:
-	for i in range(0, 9):
+	for i in range(0, board_size):
 		if  _validate_line(i) == VALIDATE.WRONG_SOLVED or\
 				_validate_row(i) == VALIDATE.WRONG_SOLVED or\
 				_validate_square(i) == VALIDATE.WRONG_SOLVED:
